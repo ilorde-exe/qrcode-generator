475bf04f7eb5d3da432904c1dc7cf009ff8bcd33
ilorde-exe <92220271+ilorde-exe@users.noreply.github.com>
2023-08-21 22:59:18 +0530

    qr download

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,54 @@
-import React, { useState } from "react";
-import QRCode from "react-qr-code";
+import React, { useState, useRef } from "react";
+import { QRCodeCanvas } from "qrcode.react";
 import "./index.css";
 
 const s = {
   bg: `flex flex-col items-center h-screen w-screen px-12 py-4  bg-gradient-to-r from-indigo-500 from-0% via-sky-500 via-35% to-emerald-500 to-100 %`,
   nav: `flex justify-center pb-12`,
   navtitle: `font-sans text-2xl font-bold text-white hover:underline decoration-2 decoration-indigo-600 underline-offset-2 drop-shadow-xl`,
-  box: `flex flex-col space-y-6 max-w-sm p-6 bg-indigo-50 border border-gray-200 rounded-lg shadow-xl `,
+  box: `flex flex-col space-y-6 max-w-sm p-6 bg-indigo-50 border border-gray-200 border-2 rounded-lg shadow-xl `,
   boxtitle: `mb-2 text-2xl font-bold tracking-tight text-indigo-600 px-2`,
   boxinput: `bg-gray-50 border-2 border-gray-300 text-gray-900 text-l font-mono rounded-lg focus:ring-indigo-600 focus:border-indigo-600 block w-full p-2.5`,
-  qr: `w-full`,
-  button: ``,
+  qr: `w-full object-cover`,
+  qrbox: `flex justify-center`,
+  button: `bg-gradient-to-r from-emerald-500 via-sky-500 to-indigo-500  hover:from-pink-500 hover:to-orange-500 text-white font-semibold px-4 py-3 rounded-md`,
 };
+
 function App() {
   const [text, setText] = useState("");
+  const qrRef = useRef();
+
+  const downloadQRCode = (e) => {
+    e.preventDefault();
+    let canvas = qrRef.current.querySelector("canvas");
+    let image = canvas.toDataURL("image/png");
+    let anchor = document.createElement("a");
+    anchor.href = image;
+    anchor.download = `${text}.png`;
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+    setUrl("");
+  };
+
   return (
     <>
       <div className={s.bg}>
         <nav className={s.nav}>
           <header className={s.navtitle}>QR-Code Generator!</header>
         </nav>
-        <article className={s.box}>
+        <form className={s.box} onSubmit={downloadQRCode}>
           <h5 className={s.boxtitle}>Enter your hyperlink/text:</h5>
-          <QRCode
-            className={s.qr}
-            value={text}
-            bgColor="#eef2ff"
-            fgColor="#4f46e5"
-          ></QRCode>
+          <div className="qrbox" ref={qrRef}>
+            <QRCodeCanvas
+              id="qrCode"
+              className="qr"
+              value={text}
+              bgColor="#eef2ff"
+              fgColor="#4f46e5"
+              level={"H"}
+            />
+          </div>
           <input
             className={s.boxinput}
             onChange={(e) => setText(e.target.value)}
@@ -35,8 +56,10 @@ function App() {
             name=""
             id=""
           />
-          <button></button>
-        </article>
+          <button type="" className={s.button}>
+            Save QR-Code
+          </button>
+        </form>
       </div>
     </>
   );
